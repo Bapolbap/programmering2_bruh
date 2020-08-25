e83b47d177c2f0b2d6f517347f93a3bf444d0afc
Fru1ty <pablo.jofre34@gmail.com>
2020-08-25 12:23:28 +0200

    Hello Niklas
    :))

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ using System.Windows.Shapes;
 
 namespace HelloWorld
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
